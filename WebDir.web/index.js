ef55a6b24a2be5b2b31c04de17845f2c24d34181
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2024-06-14 11:21:14 +0200

    initialisation de WebDir.web

---
+++ WebDir.web/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Définition du dossier des fichiers statiques
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
