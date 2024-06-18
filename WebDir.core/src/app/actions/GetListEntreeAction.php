7fbfb2e6f397d95d1a469e4716643af61800a268
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2024-06-18 23:12:12 +0200

    update csrf pour la liste des entrées

--- WebDir.core/src/app/actions/GetListEntreeAction.php
+++ WebDir.core/src/app/actions/GetListEntreeAction.php
@@ -3,24 +3,35 @@
 namespace web\directory\app\actions;
 
 use web\directory\core\services\annuaire\AnnuaireService;
+use web\directory\core\services\annuaire\AnnuaireException;
+use web\directory\app\utils\CsrfService;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
 class GetListEntreeAction extends AbstractAction{
     public function __invoke(Request $request, Response $response, array $args): Response{
+        try{
+            // view twig 
+            $view = Twig::fromRequest($request);
 
-        // view twig 
-        $view = Twig::fromRequest($request);
+            $annuaireService = new AnnuaireService();
+            $csrf_token = CsrfService::generate('entree');
 
-        $annuaireService = new AnnuaireService();
+            return $view->render($response, 'view_entree.html.twig',
+                                            [
+                                                'message' => null,
+                                                'personnes'=>$annuaireService->displayEntree(),    
+                                                'departements'=>$annuaireService->getDepartements(),
+                                                'services'=>$annuaireService->getServices(),
+                                                'csrf_token' => $csrf_token,                              
+                                            ]);
+        }catch(AnnuaireException $e){
+            return $view->render($response, 'view_entree.html.twig', ['message' => $e->getMessage(), 'csrf_token' => CsrfService::generate('entree')]);
+        }
+        catch(\Exception $e){
+            return $view->render($response, 'view_entree.html.twig', ['message' => "Impossible d'afficher les entrées (get)", 'csrf_token' => CsrfService::generate('entree')]);
+        }
 
-        return $view->render($response, 'view_entree.html.twig',
-                                        [
-                                            'personnes'=>$annuaireService->displayEntree(),    
-                                            'departements'=>$annuaireService->getDepartements(),
-                                            'services'=>$annuaireService->getServices()                                 
-                                        ]);
-       
     }
 }
--- WebDir.core/src/app/actions/PostListEntreeAction.php
+++ WebDir.core/src/app/actions/PostListEntreeAction.php
@@ -6,35 +6,58 @@ use web\directory\app\actions\AbstractAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use web\directory\core\services\annuaire\AnnuaireService;
+use web\directory\core\services\annuaire\AnnuaireException;
+use web\directory\app\utils\CsrfService;
 use Slim\Views\Twig;
 use web\directory\core\services\userData\UserDataService;
+use web\directory\core\services\userData\UserDataException;
 
 
 class PostListEntreeAction extends AbstractAction
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-
         $view = Twig::fromRequest($request);
-
+        $annuaireService = new AnnuaireService();
+        $userData = new UserDataService();
         //form data
         $data = $request->getParsedBody();
         $filteredData = filter_var_array($data, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
         $departementId = $filteredData['departement'] ?? null;
         $serviceId = $filteredData['service'] ?? null;
-        $annuaireService = new AnnuaireService();
 
-        $userData = new UserDataService();
-        if(isset($data['personne_id'])){
-            $userData->switchStatut($data['personne_id']);
+        try{
+
+            if (!isset($data['csrf_token']) || !CsrfService::check('entree', $data['csrf_token'])) {
+                throw new \Exception('CSRF token invalide');
+            }
+            
+            if(isset($data['personne_id'])){
+                $userData->switchStatut($data['personne_id']);
+            }
+
+            return $view->render($response, 'view_entree.html.twig', [
+                'message' => null,
+                'personnes' => $annuaireService->displayEntree( $departementId,$serviceId),
+                'departements' => $annuaireService->getDepartements(),
+                'services' => $annuaireService->getServices(),
+                'csrf_token' => CsrfService::generate('entree')
+            ]);
+
+        }catch(UserDataException $e){
+            return $view->render($response, 'view_entree.html.twig', ['message' => $e->getMessage(),'personnes' => $annuaireService->displayEntree( $departementId,$serviceId),
+                'departements' => $annuaireService->getDepartements(),
+                'services' => $annuaireService->getServices(), 'csrf_token' => CsrfService::generate('entree')]);
+        }catch(AnnuaireException $e){
+            return $view->render($response, 'view_entree.html.twig', ['message' => $e->getMessage(), 'personnes' => $annuaireService->displayEntree( $departementId,$serviceId),
+                'departements' => $annuaireService->getDepartements(),
+                'services' => $annuaireService->getServices(), 'csrf_token' => CsrfService::generate('entree')]);
+        }catch(\Exception $e){
+            return $view->render($response, 'view_entree.html.twig', ['message' => $e->getMessage(), 'personnes' => $annuaireService->displayEntree( $departementId,$serviceId),
+                'departements' => $annuaireService->getDepartements(),
+                'services' => $annuaireService->getServices(), 'csrf_token' => CsrfService::generate('entree')]);
         }
-
-        return $view->render($response, 'view_entree.html.twig', [
-            'personnes' => $annuaireService->displayEntree( $departementId,$serviceId),
-            'departements' => $annuaireService->getDepartements(),
-            'services' => $annuaireService->getServices(),
-        ]);
-
+        
     }
 }
